04efad0378018bff6bff20780b0b1707b6dbb1a8
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-15 15:54:14 +0100

    Add posts table migration

---
+++ database/migrations/2022_11_15_144931_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 50);
+            $table->string('content');
+            $table->bigInteger('userId')->unsigned();
+            $table->foreign('userId')->references('id')->on('users');
+            $table->bigInteger('groupId')->unsigned();
+            $table->foreign('groupId')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
