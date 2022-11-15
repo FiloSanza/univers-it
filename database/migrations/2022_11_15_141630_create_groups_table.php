15c4078b63fa50b9a7962531e7054d2c89147da7
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-15 15:31:47 +0100

    Add groups table migration

---
+++ database/migrations/2022_11_15_141630_create_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("creator")->unsigned();
+            $table->foreign("creator")->references('id')->on('users');
+            $table->string('name')->unique();
+            $table->string('description', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+};
