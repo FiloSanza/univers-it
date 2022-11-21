09eec1dfc58452dc2aab63242327f85cc5cf379a
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-21 16:20:49 +0100

    Add Group factory

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Group;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
+ */
+class GroupFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Group::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker()->word(),
+            'description' => $this->faker->paragraph(2),
+            'creator_id' => User::factory(), 
+        ];
+    }
+}
