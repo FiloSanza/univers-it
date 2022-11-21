be7cf8b82bbc2a81018c29db57658c48f5262a18
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-21 17:28:29 +0100

    Refactor `web.php`.

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Insert here all the routes that need to be authenticated.
 Route::middleware('auth')->group(function () {
+
+    // User profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::resource('/group', GroupController::class);
+    // Group routes
+    Route::get('/create-group', [GroupController::class, 'create'])->name('group.create');
+    Route::post('/create-group', [GroupController::class, 'store'])->name('group.store');
+});
 
 require __DIR__.'/auth.php';
