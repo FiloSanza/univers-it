95651818960603118d773a5e6db5d218d46339aa
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-11-24 14:48:04 +0100

    Implement CommentFactory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            // TODO Post::factory() per post_id
+            'post_id' => NULL,
+            'user_id' => User::factory(),
+            'content' => $this->faker->paragraph(2),
+        ];
+    }
+}
