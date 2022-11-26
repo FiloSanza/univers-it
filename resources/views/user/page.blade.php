32717cc8b93075962c45866275094304ebc1dbe2
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-26 16:16:27 +0100

    Add `Follow`/`Unfollow` button.

--- resources/views/user/page.blade.php
+++ resources/views/user/page.blade.php
@@ -9,6 +9,36 @@
         </h2>
     </x-slot>
 
+    @unless(Auth::id() == $user->id)
+    @if($user->followers()->where(['users.id' => Auth::id()])->first())
+    
+    {{-- TODO: Use better js --}}
+    <a href="{{ route('unfollow') }}" onclick="event.preventDefault(); document.getElementById('follow-form').submit();">
+        Unfollow
+    </a>
+
+    {{-- TODO: Use better css --}}
+    <form id="follow-form" action="{{ route('unfollow') }}" method="post" style="display: none;">
+        @csrf
+        <input type="hidden" name="followed_id" value="{{ $user->id }}" />
+    </form>
+
+    @else
+    
+    {{-- TODO: Use better js --}}
+    <a href="{{ route('follow') }}" onclick="event.preventDefault(); document.getElementById('follow-form').submit();">
+        Follow
+    </a>
+    
+    {{-- TODO: Use better css --}}
+    <form id="follow-form" action="{{ route('follow') }}" method="post" style="display: none;">
+        @csrf
+        <input type="hidden" name="followed_id" value="{{ $user->id }}" />
+    </form>
+
+    @endif
+    @endunless
+    
     <p> Followers: {{ count($user->followers()->get()) }} </p>
     <p> Follows: {{ count($user->follows()->get()) }} </p>
 
