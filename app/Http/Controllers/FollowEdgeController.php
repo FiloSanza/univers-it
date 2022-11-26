a7733b017c49b5e2bc030ebf104f8a0c92212e6b
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-26 16:18:12 +0100

    Support unfollow option in `FollowEdgeController`.

--- app/Http/Controllers/FollowEdgeController.php
+++ app/Http/Controllers/FollowEdgeController.php
@@ -15,18 +15,45 @@ class FollowEdgeController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function __invoke(Request $request)
+    public function store(Request $request)
     {
         $request->validate(FollowEdge::VALIDATION_RULES);
-        
-        //TODO: test
-        dd($request);
+        $followed_id = (int)$request->followed_id;
+
+        if ($followed_id == Auth::id()) {
+            return Redirect::back()->with('You cannot follow yourself.');
+        }
+
+        $existing_edge = FollowEdge::where(['follower_id' => Auth::id(), 'followed_id' => $followed_id])->first();
+        if ($existing_edge) {
+            return Redirect::back()->with('You already follow this user.');
+        }
 
         $edge = new FollowEdge();
         $edge->follower_id = Auth::id();
-        $edge->followed_id = $request['followed_id'];
+        $edge->followed_id = $followed_id;
         $edge->save();
 
-        return Redirect::back()->with('message', 'Success.');
+        return Redirect::back()->with('Success.');
+    }
+
+    /**
+     * Delete a follow edge.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete(Request $request)
+    {
+        $request->validate(FollowEdge::VALIDATION_RULES);
+        $followed_id = (int)$request->followed_id;
+
+        $existing_edge = FollowEdge::where(['follower_id' => Auth::id(), 'followed_id' => $followed_id])->first();
+        if ($existing_edge) {
+            $existing_edge->delete();
+            return Redirect::back()->with('Success.');
+        }
+
+        return Redirect::back()->with('You did not follow this user.');
     }
 }
