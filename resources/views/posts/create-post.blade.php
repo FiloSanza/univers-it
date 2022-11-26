f7ad1350fe405fb23b2e752aa5e0cddbf2c67fc6
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-11-26 16:32:03 +0100

    Add post views.

---
+++ resources/views/posts/create-post.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create Post') }}
+        </h2>
+    </x-slot>
+
+    <form action="/create-post" method="post">
+        @csrf
+        <x-input-label for="title">
+            Post Title
+        </x-input-label>
+        <x-text-input 
+            id="title" 
+            name="title"
+            type="text"
+            class="block mt-1 w-full"
+            required />
+        <x-input-label for="content">
+            Post Content
+        </x-input-label>
+        <x-textarea 
+            id="content" 
+            name="content"
+            rows="4"
+            maxlength="500"
+            class="block mt-1 w-full" 
+            placeholder="Insert the post's content..."
+            required/>
+        <x-primary-button>
+            Confirm
+        </x-primary-button>
+    </form>
+</x-app-layout>
