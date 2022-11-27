2aa511fbd2cc822a162275b03ff528a5722718ac
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-11-27 15:00:56 +0100

    Add `PostFactory.php`.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Group;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\app\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the post's default state.
+     *
+     * @return array<string, string>
+     */
+    public function definition()
+    {
+        return [
+            'creator_id' => User::factory(),
+            'group_id' => Group::factory(),
+            'title' => fake()->word(),
+            'content' => fake()->paragraph(2)
+        ];
+    }
+}
