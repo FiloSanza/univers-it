c9b35bc0754d87b591c2b34c45bbd75a6ec48337
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-29 14:13:06 +0100

    Add comment count in small post.

--- resources/views/components/posts/small.blade.php
+++ resources/views/components/posts/small.blade.php
@@ -4,13 +4,17 @@
         - Title
         - Group (if set)
         - User (if set)
+        - Comment count
 --}}
 @props([ 'post', 'group' => null, 'user' => null ])
 
+@php($comment_count = $post->comments()->count())
+
 <div>
-    <h2> <a href="{{ route('post.show', $post->id) }}"> {{ $post->title }} </a> </h2>
+    <h2> <a href="{{ route('post.show', $post->id) }}"> {{ $post->title }} </a> </h2> 
+    <small> Comments: {{ $comment_count }} </small>
     @if($group)
-        <small><a href="{{ route('group.show', $group->name) }}"> Posted in {{ $group->name }} </a></small>
+        <small><a href="{{ route('group.show', $group->name) }}"> - Posted in {{ $group->name }} </a></small>
     @endif
     @if($user)
         <small><a href="{{ route('userpage.show', $user->name) }}"> By {{ $user->name }} </a></small>
