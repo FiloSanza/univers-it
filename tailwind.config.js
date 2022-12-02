0e6c38a076b9fe22f24742c90d3f82fded3be3e2
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-02 15:02:59 +0100

    Update config files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,21 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+    },
+    variants: {
     },
 
     plugins: [require('@tailwindcss/forms')],
