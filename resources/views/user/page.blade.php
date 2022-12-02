75a8a28779731082a7604549eb3f81b2d4816b67
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-12-02 16:54:45 +0100

    User page header

--- resources/views/user/page.blade.php
+++ resources/views/user/page.blade.php
@@ -12,13 +12,34 @@
 @endphp
 
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ $user->name }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
+    <div class="max-w-5xl mx-auto">
+        <div class="grid grid-flow-col">
+            <div class="bg-blue-700"></div> {{-- Img --}}
+            <div class="bg-red-400 col-span-3">
+                <p class="font-bold text-2xl"> {{ $user->name }} </p>
+                <p class="font-semibold"> Università di Bologna </p>
+            </div>
+            <div class="bg-green-300 text-right grid grid-rows-2">
+                <div>
+                    <p> {{count($posts)}} <small>Posts</small>  </p>
+                    <p> {{count($followers)}} <small>Followers</small> </p>
+                    <p> {{count($follows)}} <small>Following</small> </p>
+                </div>
+                <div>
+                    @unless(Auth::id() == $user->id)
+                        <x-users.follow-button :userid="$user->id" :isfollowed="$already_followed" />
+                    @endunless
+                </div>
+            </div>
+        </div>
+    </div>
 
-    @unless(Auth::id() == $user->id)
+    {{-- @unless(Auth::id() == $user->id)
         <x-users.follow-button :userid="$user->id" :isfollowed="$already_followed" />
     @endunless
     
@@ -26,7 +47,7 @@
     <p> Follows: {{ count($follows) }} </p>
     
     <br>
-    <h3> Followers </h3>
+    <h3 class="bg-blue-300"> Followers </h3>
     <x-list.list itemtemplate='components.users.small' :items="$followers->map($user_lambda)" />
     
     <br>
@@ -35,7 +56,7 @@
         
     <br>
     <h3> POSTS </h3>
-    <x-list.list itemtemplate='components.posts.small' :items="$posts->map($post_lambda)" />
+    <x-list.list itemtemplate='components.posts.small' :items="$posts->map($post_lambda)" /> --}}
 
 
 
