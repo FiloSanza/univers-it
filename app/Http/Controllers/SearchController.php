8428633a60e9e4fb7d91217fba15c1692a98333c
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-12-05 18:17:00 +0100

    Use invokable controller.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Group;
 use App\Models\User;
+use App\Models\Group;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-
     /**
      * Display the users and the groups whose names contains the text input.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\View\View
      */
-    public function show(Request $request)
-    { 
+    public function __invoke(Request $request)
+    {
         $name = $request->name;
 
         $results['users'] = User::query()
@@ -29,5 +28,4 @@ class SearchController extends Controller
 
         return view('search.search', ['substring' => $name, 'results' => $results]);
     }
-
 }
