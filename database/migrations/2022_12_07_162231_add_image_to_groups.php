3743e96c916f5a7c685ab58f5256c8dd5313a205
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-07 17:35:33 +0100

    Add migration to add group images.

---
+++ database/migrations/2022_12_07_162231_add_image_to_groups.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->foreignId('image')
+                ->default('862e0b2dea8474f9a52c5e3a5cef9afb6bd971099b8c09daaadcfd77f1c1df7f.png')
+                ->constrained('images')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->dropColumn('image');
+        });
+    }
+};
