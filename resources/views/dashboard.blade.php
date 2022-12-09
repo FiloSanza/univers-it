78d8f8eda1999324c4431b5a8026e3959760b6d1
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-12-09 23:30:42 +0100

    Change dashboard view.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,10 +15,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900">
                     You're logged in!
-                </div> -->
-                <x-list.list itemtemplate='components.posts.small' :items="$feed->map($list_lambda)" />
+                </div>
+
+                <br/>
+
+                <div class="p-6 text-gray-900">
+                    <x-list.list itemtemplate='components.posts.small' :items="$feed->map($list_lambda)" />
+                </div>
             </div>
         </div>
     </div>
