d5150b83c773edfcda69b76328fe0d60f9eb7b4a
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-12-09 15:37:20 +0100

    Add `DashboardController.php`.

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\Models\User;
+use \App\Models\Group;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use PHPUnit\TextUI\XmlConfiguration\Groups;
+
+class DashboardController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $user = Auth::user();
+        $follows = $user->follows()->get();
+        $groups = $user->followed_groups()->get();
+        $feed = new Collection();
+
+        foreach ($groups as $group) {
+            $posts = $group->posts()->get();
+            foreach ($posts as $post) {
+                $feed->add($post);
+            }
+        }
+        foreach ($follows as $follow) {
+            
+            $posts = $follow->posts()->get();
+            foreach ($posts as $post) {
+                $feed->add($post);
+            }
+        }
+
+
+        $feed->unique();
+        $sorted_feed = $feed->sortByDesc(function($post)
+        {
+            return strtotime($post->created_at);
+        });
+        return view('dashboard', ['feed' => $sorted_feed]);
+    }
+}
