bc9ffe56edf720badda3cafce0994e054e383d80
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-09 23:21:25 +0100

    Add Style to small post views.

--- resources/views/user/page.blade.php
+++ resources/views/user/page.blade.php
@@ -7,15 +7,15 @@
     
     $user_lambda = function ($f) { return [ 'user' => $f ]; };
     $post_lambda = function ($p) { 
-        return [ 'post' => $p, 'group' => $p->group()->first() ]; 
+        return [ 'post' => $p, 'group' => $p->group()->first(), 'user' => $p->user()->first() ]; 
     };
 @endphp
 
 <x-app-layout>
-    <div class="mx-auto lg:max-w-5xl">
+    <header class="mx-auto border-b-2 border-gray-600 p-3 lg:max-w-5xl">
         <div class="flex flex-col items-center lg:flex-row">
             <div class="lg:w-1/5">
-                <img src="{{ route('image.get', $user->propic) }}" class="mx-auto w-24 h24" />
+                <img src="{{ route('image.get', $user->propic) }}" class="mx-auto w-24 h24 rounded-full" />
             </div>
             <div class="text-center lg:w-3/5">
                 <div class="font-bold text-2xl"> {{ $user->name }} </div>
@@ -27,7 +27,9 @@
                         <p> {{ count($posts) }} </p> 
                         <small>Posts</small>
                     </div>
-                    <div class="text-center float-left m-1">
+                    <div 
+                        class="text-center float-left m-1"
+                        onclick="(e) => e.preventDefault(); document.dispatchEvent(new Event('open-modal', {'detail': 'followers-modal'}));" >
                         <p> {{ count($followers) }} </p> 
                         <small>Followers</small>
                     </div>
@@ -43,27 +45,13 @@
                 @endunless
             </div>
         </div>
-    </div>
+    </header>
 
-    {{-- @unless(Auth::id() == $user->id)
-        <x-users.follow-button :userid="$user->id" :isfollowed="$already_followed" />
-    @endunless
-    
-    <p> Followers: {{ count($followers) }} </p>
-    <p> Follows: {{ count($follows) }} </p>
-    
-    <br>
-    <h3 class=""> Followers </h3>
-    <x-list.list itemtemplate='components.users.small' :items="$followers->map($user_lambda)" />
-    
-    <br>
-    <h3> Follows </h3>
-    <x-list.list itemtemplate='components.users.small' :items="$follows->map($user_lambda)" />
-        
-    <br>
-    <h3> POSTS </h3>
-    <x-list.list itemtemplate='components.posts.small' :items="$posts->map($post_lambda)" /> --}}
-
-
+    <section class="mx-auto mt-5 p-5 lg:w-4/5">
+        <div class="w-full text-center text-2xl">
+            <h3> POSTS </h3>
+        </div>
+        <x-list.list itemtemplate='components.posts.small' :items="$posts->map($post_lambda)" />
+    </section>
 
 </x-app-layout>
