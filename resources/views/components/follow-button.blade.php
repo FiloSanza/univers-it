569d22500b9c4e50e5ba9ab5ce4294f16ca73c98
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-09 14:49:36 +0100

    Add hover effect on follow button

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -6,13 +6,15 @@
         ? ($isfollowed ? route('user.unfollow') : route('user.follow'))
         : ($isfollowed ? route('group.unfollow') : route('group.follow'));
     $text = $isfollowed ? 'Unfollow' : 'Follow';
-    $classes = $isfollowed ? 'bg-red-500 text-white rounded-xl px-10 py-0.5' : 'bg-blue-500 text-white rounded-xl px-10 py-0.5';
+    $classes = $isfollowed
+        ? "bg-red-500 text-white rounded-xl px-10 py-0.5 hover:bg-white hover:text-red-500 hover:border-2 hover:border-red-500"
+        : "bg-blue-500 text-white rounded-xl px-10 py-0.5 hover:bg-white hover:text-blue-500 hover:border-2 hover:border-blue-500";
 @endphp
 
 <x-post-request-button 
     id='{{ $id }}'
     :route='$route'
     :parameters="$groupid ? ([ 'group_id' => $groupid ]) : ([ 'followed_id' => $userid ])"
-    class='{{ $classes }}' >
+    :class='$classes' >
     {{ $text }}
 </x-post-request-button>
