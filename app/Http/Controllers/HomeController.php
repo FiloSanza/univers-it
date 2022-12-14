3c8147fe831b8eef099da3456efd1b64c0e82197
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-14 17:54:10 +0100

    Remove unused import and fix warning.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     /**
      * Return the user's feed.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function __invoke()
     {
