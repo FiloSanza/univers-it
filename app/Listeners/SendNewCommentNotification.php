b66c52df0efbd38583e3ee8c49eb8b2bd36ae6a9
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-14 18:19:30 +0100

    Add `NewComment` event.

---
+++ app/Listeners/SendNewCommentNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewCommentEvent;
+use App\Notifications\NewCommentNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendNewCommentNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\NewCommentEvent  $event
+     * @return void
+     */
+    public function handle(NewCommentEvent $event)
+    {
+        $user = $event->comment->post()->first()->user()->first();
+        $user->notify(new NewCommentNotification($user, $event->comment));
+    }
+}
