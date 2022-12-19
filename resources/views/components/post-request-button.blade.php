659d3792fec82ea9c721dd723ca5d226d161bc4c
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-19 16:31:50 +0100

    Change css in `post-request-button`.

--- resources/views/components/post-request-button.blade.php
+++ resources/views/components/post-request-button.blade.php
@@ -4,16 +4,11 @@
 
 <div>
     {{-- TODO: Use better js --}}
-    <a 
-        href="{{ $route }}" 
-        onclick="event.preventDefault(); document.getElementById('{{ $id }}').submit();"
-        class='{{ $attributes->get('class') }}' 
-    >
+    <a href="{{ $route }}" onclick="event.preventDefault(); document.getElementById('{{ $id }}').submit();" class='{{ $attributes->get('class') }}' >
         {{ $slot }}
     </a>
 
-    {{-- TODO: Use better css --}}
-    <form id="{{ $id }}" action="{{ $route }}" method="post" style="display: none;">
+    <form id="{{ $id }}" action="{{ $route }}" method="post" class="hidden">
         @csrf
         @foreach ($parameters as $key => $value)
             <input type="hidden" name="{{ $key }}" value="{{ $value }}" />
