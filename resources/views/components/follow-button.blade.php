db73474e74c71d04e4631645c0ca14e8b12f140b
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-19 16:32:22 +0100

    Make `follow-button` ids unique.

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,14 +1,14 @@
 @props([ 'userid' => null, 'groupid' => null, 'isfollowed' ])
 
 @php
-    $id = $isfollowed ? 'unfollow-form' : 'follow-form';
+    $id = ($isfollowed ? 'unfollow-form' : 'follow-form').'-'.Str::random(10);
     $route = $userid 
         ? ($isfollowed ? route('user.unfollow') : route('user.follow'))
         : ($isfollowed ? route('group.unfollow') : route('group.follow'));
     $text = $isfollowed ? 'Unfollow' : 'Follow';
     $classes = $isfollowed
-        ? "bg-red-500 text-white rounded-xl px-8 py-0.5 hover:bg-white hover:text-red-500 hover:border-2 hover:border-red-500"
-        : "bg-blue-500 text-white rounded-xl px-10 py-0.5 hover:bg-white hover:text-blue-500 hover:border-2 hover:border-blue-500";
+        ? "bg-red-500 text-white rounded-xl px-8 py-0.5 border-2 border-red-500 hover:bg-red-100 hover:text-red-500"
+        : "bg-blue-500 text-white rounded-xl px-10 py-0.5 border-2 border-blue-500 hover:bg-blue-100 hover:text-blue-500";
 @endphp
 
 <x-post-request-button 
