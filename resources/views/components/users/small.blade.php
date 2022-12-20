576160ef2469170a39b0eabab54d47102d7436f2
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-20 09:52:03 +0100

    Style small post and user.

--- resources/views/components/users/small.blade.php
+++ resources/views/components/users/small.blade.php
@@ -1,12 +1,12 @@
 @props([ 'user', 'already_followed' => null ])
-<div class="flex flex-row items-center">
+<div class="flex flex-row items-center p-1 hover:bg-gray-200 hover:rounded-xl">
     <div class="flex flex-row w-2/3">
-        <img src="{{ route('image.get', $user->propic) }}" class="w-12 rounded-full h-12 mx-2 my-2">
+        <img src="{{ route('image.get', $user->propic) }}" class="w-12 rounded-full h-12 mx-2 my-2" alt="{{ $user->name }}" />
         <a href="{{ route('userpage.show', $user->name) }}" class="font-bold my-5"> 
             {{ $user->name }} 
         </a>
     </div>
-    @unless(is_null($already_followed))
+    @unless(is_null($already_followed) || Auth::id() === $user->id)
         <div class="w-1/3 flex flex-row-reverse">
             <x-follow-button :userid="$user->id" :isfollowed="$already_followed" />
         </div>
--- resources/views/groups/group.blade.php
+++ resources/views/groups/group.blade.php
@@ -16,7 +16,7 @@
 <x-app-layout>
     <div class="flex flex-col items-center lg:flex-row border-b-2 border-gray-600 p-3">
         <div class="lg:w-1/5">
-            <img class="mx-auto w-24 h-24 rounded-full" src="{{ route('image.get', $group->image_id) }}" />
+            <img class="mx-auto w-24 h-24 rounded-full" src="{{ route('image.get', $group->image_id) }}" alt="{{ $group->name }} profile picture." />
         </div>
         <div class="text-center lg:w-3/5">
             <h1 class="font-bold text-2xl">
@@ -24,23 +24,23 @@
             </h1>
             <p> {{ $group->description }} </p>
         </div>
-        <div class="flex flex-col lg:w-1/5 items-center">
+        <div class="my-2 w-4/5 flex items-center flex-row lg:flex-col lg:w-1/5">
             @auth
-                <x-follow-button :groupid="$group->id" :isfollowed="$already_followed"/>
-                <div class="mt-2">
-                    <a class="bg-blue-500 text-white rounded-xl px-12 py-0.5 hover:bg-white hover:text-blue-500 hover:border-2 hover:border-blue-500"
-                        href="{{ route('post.create', $group->name) }}"
-                        >
+                <div class="w-1/2 text-center lg:w-full lg:my-2">
+                    <x-follow-button :groupid="$group->id" :isfollowed="$already_followed"/>
+                </div>
+                <div class="w-1/2 text-center lg:w-full lg:my-2">
+                    <a class="bg-blue-500 text-white rounded-xl px-12 py-0.5 hover:bg-white hover:text-blue-500 hover:border-2 hover:border-blue-500" href="{{ route('post.create', $group->name) }}">
                         Post
                     </a>
-                </div> 
+                </div>
             @endauth
         </div>
     </div>
     <section class="mx-auto mt-5 p-5 lg:w-4/5">
-        <div class="w-full text-center text-2xl">
-            <h3> POSTS </h3>
-        </div>
+        <h2 class="w-full text-center text-2xl">
+            POSTS
+        </h2>
         <x-list.list itemtemplate='components.posts.small' :items="$posts->map($list_lambda)" />
     </section>
 
