97a8ffbb43cc9fbbf3f20054ed6c922bc8a8d7bc
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-22 11:15:16 +0100

    Fix hover colors in comments.

--- resources/views/components/comments/small.blade.php
+++ resources/views/components/comments/small.blade.php
@@ -7,17 +7,17 @@
 --}}
 
 <div class="my-1">
-    <div id="{{ $id }}" class="p-3 bg-gray-100 rounded-xl target:bg-fuchsia-100">
+    <div id="{{ $id }}" class="p-3 bg-gray-100 rounded-xl target:bg-gra00">
         <div class="flex">
             <img src="{{ route('image.get', $user->propic) }}" class="w-7 h-7 rounded-full mx-2" />
             <a class="font-bold mx-2" href="{{ route('userpage.show', $user->name) }}"> {{ $user->name }} </a>
             @if($reply_to)
-            <a class="p-1 w-20 text-center font-bold bg-blue-100 rounded-xl ml-auto hover:bg-fuchsia-100" href="#{{ $reply_to }}">Previous</a>
+            <a class="p-1 w-20 text-center font-bold bg-blue-100 rounded-xl ml-auto hover:bg-blue-300" href="#{{ $reply_to }}">Previous</a>
             @endif
         </div>
         <div class="mt-2 flex">
             <a class="mx-4"> {{ $content }} </a> 
-            <p class="reply-span w-20 ml-auto p-1 text-center font-bold bg-blue-100 rounded-xl hover:bg-fuchsia-100" data-target="comment-modal">Reply</p>
+            <p class="reply-span w-20 ml-auto p-1 text-center font-bold bg-blue-100 rounded-xl hover:bg-blue-300" data-target="comment-modal">Reply</p>
         </div>
     </div>
 </div>
