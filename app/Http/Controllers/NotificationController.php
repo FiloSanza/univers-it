f959815624cf05a20c2cf420be75db03199978e5
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-12-22 14:36:04 +0100

    Add endpoint for ajax notifications.

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\ViewModels\NotificationViewModel;
+use Illuminate\Support\Collection;
+use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Response;
 
 class NotificationController extends Controller
 {
@@ -15,7 +17,7 @@ class NotificationController extends Controller
      * @return Response
      */
     public function markAsRead(string $id) {
-        if ($notification = Auth::user()->unreadNotifications()->where('id', $id)) {
+        if ($notification = Auth::user()->unreadNotifications()->where('id', $id)->first()) {
             $notification->markAsRead();
             return Response('Success.', 200);
         }
@@ -31,5 +33,50 @@ class NotificationController extends Controller
     public function readAll() {
         Auth::user()->unreadNotifications()->update(['read_at' => now()]);
         return Response('Success.', 200);
+    }
+
+    /**
+     * Returns all the unread notifications, and the last 10 read notifications.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getNotifications() {
+        $map_to_view_models = function (DatabaseNotification $n) {
+            return ['notification' => new NotificationViewModel($n)];
+        };
+        $get_view = function (Collection $list) {
+            return view('components.list.list', [
+                'itemtemplate' => 'components.notifications.small',
+                'items' => $list
+            ])->render();
+        };
+
+        $read_notifications = Auth::user()
+            ->notifications()
+            ->latest()
+            ->whereNotNull('read_at')
+            ->limit(10)
+            ->get()
+            ->map($map_to_view_models);
+        $unread_notifications = Auth::user()
+            ->unreadNotifications()
+            ->latest()
+            ->get()
+            ->map($map_to_view_models);
+
+        return response()->json([
+            'read' => $get_view($read_notifications),
+            'unread' => $get_view($unread_notifications),
+            'unread_count' => $unread_notifications->count()
+        ]);
+    }
+
+    /**
+     * Show notifications page.
+     * 
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function showNotificationsPage() {
+        return view('notifications.page');
     }
 }
