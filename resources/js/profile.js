c5cfae2795be0c9a89d2e50274ae59742fa2abb6
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-01-03 22:10:51 +0100

    Add ajax to profile's mail settings.

--- resources/js/profile.js
+++ resources/js/profile.js
@@ -1,11 +1,31 @@
+import axios from "axios";
 import $ from 'jquery';
+
+const loadMailSetting = function () {
+    axios.get('/profile/mail-settings').then(response => {
+        for (const key in response.data) {
+            const value = response.data[key];
+            $('#' + key).prop('checked', value);
+        }
+    });
+}
 
 const handle_cancel_click = function() {
     $('.popup-body').hide();
 }
 
+const handle_checkbox = function(evt) {
+    evt.preventDefault();
+    const type = evt.target.id;
+    const data = { 'type':type };
+    axios.patch('/profile/mail-settings',data)
+        .then( () => loadMailSetting() );
+}
+
 $(() => {
+    loadMailSetting();
     $('#cancel-button').on('click', handle_cancel_click);
+    $('input[type="checkbox"]').on('change', handle_checkbox);
 
     setTimeout(function() {
         $('.value-updated').fadeOut('slow');
--- resources/views/profile/partials/mail-settings.blade.php
+++ resources/views/profile/partials/mail-settings.blade.php
@@ -5,26 +5,26 @@
     </header>
 
     <div class="p-1 items-center">
-        <input type="checkbox" id="new-post" name="new-post" data-target="new-post-mail" class="m-1" />
-        <x-input-label for="new-post" class="inline-block m-1">
+        <input type="checkbox" id="new_post" name="new_post" data-target="new-post-mail" class="m-1" />
+        <x-input-label for="new_post" class="inline-block m-1">
             New post notifications.
         </x-input-label>
     </div>
     <div class="p-1 items-center">
-        <input type="checkbox" id="new-follower" name="new-follower" data-target="new-follower-mail" class="m-1" />
-        <x-input-label for="new-follower" class="inline-block m-1">
+        <input type="checkbox" id="new_follower" name="new_follower" data-target="new-follower-mail" class="m-1" />
+        <x-input-label for="new_follower" class="inline-block m-1">
             New follower notifications.
         </x-input-label>
     </div>
     <div class="p-1 items-center">
-        <input type="checkbox" id="new-comment" name="new-comment" data-target="new-comment-mail" class="m-1" />
-        <x-input-label for="new-comment" class="inline-block m-1">
+        <input type="checkbox" id="new_comment" name="new_comment" data-target="new-comment-mail" class="m-1" />
+        <x-input-label for="new_comment" class="inline-block m-1">
             New comment notifications.
         </x-input-label>
     </div>
     <div class="p-1 items-center">
-        <input type="checkbox" id="new-reaction" name="new-reaction" data-target="new-reaction-mail" class="m-1" />
-        <x-input-label for="new-reaction" class="inline-block m-1">
+        <input type="checkbox" id="new_reaction" name="new_reaction" data-target="new-reaction-mail" class="m-1" />
+        <x-input-label for="new_reaction" class="inline-block m-1">
             New reaction notifications.
         </x-input-label>
     </div>
