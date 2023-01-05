287613f792332349ef159d61205d650f14d7fe4a
Rachele Margutti <rachele.margutti@studio.unibo.it>
2023-01-05 15:01:56 +0100

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="{{ url('logo.png') }}" />
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
