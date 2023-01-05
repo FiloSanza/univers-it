000826ee02838f195fd6d0d24bb53250b464c12a
Rachele Margutti <rachele.margutti@studio.unibo.it>
2023-01-05 16:50:34 +0100

    reactions frontend

--- resources/js/like.js
+++ resources/js/like.js
@@ -1,9 +1,45 @@
 import $ from 'jquery';
 
-const handle_click = function(button_name) {
-    
+let post_id;
+
+const update_reactions = function() {
+    $.get(`/post/reactions/${post_id}`)
+        .then(response => {
+            console.log(response);
+            $("#like-number").html(response.likes);
+            $("#dislike-number").html(response.dislikes);
+            if(response.user_reaction) {
+                $(`#${response.user_reaction}-button`).focus(); 
+            }
+        })
+        .catch(err => console.log(err));
 }
 
-$('#like-button').on('click', function (evt) {
-    handle_click('like');
+const handle_click = function(button_name) {
+    $.post("/post/react", { "post_id": post_id, "reaction_name": button_name })
+        .then(response => {
+            console.log(response);
+            update_reactions();
+        })
+        .catch(err => console.log(err));
+}
+
+$(() => {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    post_id = $('div[data-post]').data('post');
+
+    $('#like-button').on('click', function (evt) {
+        handle_click('like');
+    });
+    
+    $('#dislike-button').on('click', function (evt) {
+        handle_click('dislike');
+    });
+
+    update_reactions();
 });
