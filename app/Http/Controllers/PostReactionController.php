5e4ed948c8b32b632582b811b362a4c1aee6b724
Rachele Margutti <rachele.margutti@studio.unibo.it>
2023-01-05 16:50:19 +0100

    Set reactions

--- app/Http/Controllers/PostReactionController.php
+++ app/Http/Controllers/PostReactionController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\PostReaction;
+use App\Models\Reaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostReactionController extends Controller
 {
@@ -15,13 +17,28 @@ class PostReactionController extends Controller
      */
     public function store(Request $request)
     {
+        // dd($request);
         $request->validate(PostReaction::VALIDATION_RULES);
+        $reaction_id = Reaction::where('name', $request->reaction_name)->first()->id;
+
+        // Find previous reactions if any.
+        $old = PostReaction::where(['user_id' => Auth::id(), 'post_id' => $request->post_id])->first();
+        // Remember is user is setting or removing the reaction based on the previous one if present.
+        $was_set = isset($old) && $old->reaction_id == $reaction_id;
+
+        // Remove old reaction.
+        if (isset($old)) {
+            $old->delete();
+        }
+
+        if ($was_set) {
+            return Response('Success.', 200);
+        }
 
         $post_reaction = new PostReaction();
-
         $post_reaction->user_id = Auth::id();
         $post_reaction->post_id = $request->post_id;
-        $post_reaction->reaction_id = PostReaction::where('name', $request->reaction_name)->first()->id;
+        $post_reaction->reaction_id = $reaction_id;
 
         $post_reaction->save();
 
@@ -30,8 +47,8 @@ class PostReactionController extends Controller
 
     public function getInfo(int $id)
     {
-        $like_reaction = Reactions::where('name', 'like')->first();
-        $dislike_reaction = Reactions::where('name', 'dislike')->first();
+        $like_reaction = Reaction::where('name', 'like')->first();
+        $dislike_reaction = Reaction::where('name', 'dislike')->first();
 
         $likes = PostReaction::where([ 'post_id' => $id, 'reaction_id' => $like_reaction->id ])->get()->count();
         $dislikes = PostReaction::where([ 'post_id' => $id, 'reaction_id' => $dislike_reaction->id ])->get()->count();
