e044935c62ea3a3fa9b509ac0e8708357c649971
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2023-01-06 17:00:24 +0100

    Color like button when clicked.

--- resources/js/like.js
+++ resources/js/like.js
@@ -2,14 +2,30 @@ import $ from 'jquery';
 
 let post_id;
 
+const bg_hover = 'rgb(199, 206, 217)';
+const bg_clicked = 'rgb(157, 193, 250)';
+const bg = 'rgb(203, 220, 247)';
+
+let clicked;
+let not_clicked;
+
 const update_reactions = function() {
     $.get(`/post/reactions/${post_id}`)
         .then(response => {
-            console.log(response);
             $("#like-number").html(response.likes);
             $("#dislike-number").html(response.dislikes);
             if(response.user_reaction) {
-                $(`#${response.user_reaction}-button`).focus(); 
+                clicked = response.user_reaction;
+                not_clicked = response.user_reaction === 'like' ? 'dislike' : 'like';
+
+                $(`#${clicked}-button`).css('background-color', bg_clicked);
+                $(`#${not_clicked}-button`).css('background-color', bg);
+            } else {
+                clicked = null;
+                not_clicked = null;
+
+                $("#like-button").css('background-color', bg);
+                $("#dislike-button").css('background-color', bg);
             }
         })
         .catch(err => console.log(err));
@@ -31,14 +47,33 @@ $(() => {
         }
     });
 
+    $('#like-button').css('background-color', bg);
+    $('#dislike-button').css('background-color', bg);
+
     post_id = $('div[data-post]').data('post');
 
     $('#like-button').on('click', function (evt) {
         handle_click('like');
     });
-    
+
     $('#dislike-button').on('click', function (evt) {
         handle_click('dislike');
+    });
+
+    $('#like-button').on('mouseenter', function (evt) {
+        $(this).css('background-color', bg_hover);
+    });
+
+    $('#like-button').on('mouseleave', function (evt) {
+        $(this).css('background-color', clicked === 'like' ? bg_clicked : bg);
+    });
+
+    $('#dislike-button').on('mouseenter', function (evt) {
+        $(this).css('background-color', bg_hover);
+    });
+
+    $('#dislike-button').on('mouseleave', function (evt) {
+        $(this).css('background-color', clicked === 'dislike' ? bg_clicked : bg);
     });
 
     update_reactions();
